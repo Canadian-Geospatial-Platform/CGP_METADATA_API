af2c62ed897a5ee7aa75b37aa295cb4fcf7e9f24
Pascal Vautour <vautour.pascal@gmail.com>
2020-02-25 13:48:56 -0500

    getAll works

--- handler.js
+++ handler.js
@@ -4,23 +4,33 @@ const aws = require("aws-sdk");
 const athenaExpressConfig = {
   aws,
   s3: "s3://cgp-metadata-search-dev",
-  getStats: true
+  getStats: true,
+  formatJson: false
 };
 
 const athenaExpress = new AthenaExpress(athenaExpressConfig);
 
 //Invoking a query on Amazon Athena
 export async function getAll(event) {
+  // let Bbox =
+  // '{type=Polygon, coordinates=[[["-127.737823","52.127108"],["-102.774389","52.127108"],["-102.774389","65.165894"],["-127.737823","65.165894"],["-127.737823","52.127108"]]]}';
   let myQuery = {
-    sql: "SELECT type, geometry, properties FROM metadata LIMIT 3",
+    sql: "SELECT * FROM metadata LIMIT 10",
     db: "cgp-metadata-search-dev"
   };
 
   try {
     let results = await athenaExpress.query(myQuery);
-    console.log(results);
-  } catch (error) {
-    console.log(error);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not fetch results."
+    };
   }
 }
 
