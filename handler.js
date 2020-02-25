6e1d4b04ba320775b0c7d0510e7ee237adea4631
Pascal Vautour <vautour.pascal@gmail.com>
2020-02-25 15:18:25 -0500

    output bucket is now different from inpu bucket

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ const aws = require("aws-sdk");
 
 const athenaExpressConfig = {
   aws,
-  s3: "s3://cgp-metadata-search-dev",
+  s3: "s3://cgp-metadata-search-dev-athena-output",
   getStats: true,
   formatJson: true
 };
@@ -15,7 +15,8 @@ export async function getAll(event) {
   // let Bbox =
   // '{type=Polygon, coordinates=[[["-127.737823","52.127108"],["-102.774389","52.127108"],["-102.774389","65.165894"],["-127.737823","65.165894"],["-127.737823","52.127108"]]]}';
   let myQuery = {
-    sql: "SELECT * FROM metadata LIMIT 10",
+    sql:
+      "SELECT * FROM metadata where properties.id = '05870f11-a52a-4bf4-bc15-910fd0b8a1a3' limit 10",
     db: "cgp-metadata-search-dev"
   };
 
