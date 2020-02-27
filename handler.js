2823cffffebf185b77858583626d506bcd10d26c
Pascal Vautour <vautour.pascal@gmail.com>
2020-02-27 14:52:43 -0500

    we can now do regex based text searches on level 1 metadata.

--- handler.js
+++ handler.js
@@ -1,26 +1,50 @@
+import flatten from "flat";
 const AthenaExpress = require("athena-express");
 const aws = require("aws-sdk");
 
 const athenaExpressConfig = {
   aws,
-  s3: "s3://cgp-metadata-search-dev",
-  getStats: true
+  s3: "s3://cgp-metadata-search-dev-athena-output",
+  getStats: true,
+  formatJson: true
 };
 
 const athenaExpress = new AthenaExpress(athenaExpressConfig);
 
-//Invoking a query on Amazon Athena
 export async function getAll(event) {
-  let myQuery = {
-    sql: "SELECT type, geometry, properties FROM metadata LIMIT 3",
-    db: "cgp-metadata-search-dev"
-  };
-
   try {
+    let input = JSON.parse(event.body);
+    const allowed = ["properties"];
+    Object.keys(input)
+      .filter(key => !allowed.includes(key))
+      .forEach(key => delete input[key]);
+    let where = "";
+
+    const flattenedInput = flatten(input);
+    for (let key in flatten(flattenedInput)) {
+      if (where) {
+        where += " AND ";
+      }
+      where += "regexp_like(" + key + ",'" + flattenedInput[key] + "')";
+    }
+
+    let myQuery = {
+      sql: "SELECT * FROM metadata WHERE " + where + " limit 10;",
+      db: "cgp-metadata-search-dev"
+    };
+
     let results = await athenaExpress.query(myQuery);
-    console.log(results);
-  } catch (error) {
-    console.log(error);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not fetch results."
+    };
   }
 }
 
